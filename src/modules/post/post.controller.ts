082ef971da8ca61b22338c756f4bc738ceaeda69
Fahim Ahammed Firoz <fahimahammed.cse@gmail.com>
2025-09-12 19:18:28 +0600

    part-2 prisma basics

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -12,7 +12,13 @@ const createPost = async (req: Request, res: Response) => {
 
 const getAllPosts = async (req: Request, res: Response) => {
     try {
-        const result = await PostService.getAllPosts();
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const search = (req.query.search as string) || "";
+        const isFeatured = req.query.isFeatured ? req.query.isFeatured === "true" : undefined
+        const tags = req.query.tags ? (req.query.tags as string).split(",") : []
+
+        const result = await PostService.getAllPosts({ page, limit, search, isFeatured, tags });
         res.json(result);
     } catch (err) {
         res.status(500).json({ error: "Failed to fetch posts", details: err });
@@ -35,10 +41,21 @@ const deletePost = async (req: Request, res: Response) => {
     res.json({ message: "Post deleted" });
 };
 
+
+const getBlogStat = async (req: Request, res: Response) => {
+    try {
+        const result = await PostService.getBlogStat();
+        res.json(result);
+    } catch (err) {
+        res.status(500).json({ error: "Failed to fetch stats", details: err });
+    }
+};
+
 export const PostController = {
     createPost,
     getAllPosts,
     getPostById,
     updatePost,
-    deletePost
+    deletePost,
+    getBlogStat
 }
--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { PostController } from './post.controller';
 
 const router = express.Router();
+router.get("/stats", PostController.getBlogStat)
 
 router.post(
     "/",
